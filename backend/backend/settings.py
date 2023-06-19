d86b0f3c790454324b8e125dad505314829c8d70
angelikaDe <angelikademina@yandex.ru>
2023-06-19 11:16:37 +0300

    with tele message

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALOWED_HOSTS = ['158.160.31.32', '127.0.0.1', 'localhost', 'yakittygram.hopto.org']
+ALOWED_HOSTS = ['158.160.31.32', '127.0.0.1', 'localhost', 'yaproject.hopto.org']
 
 # Application definition
 
